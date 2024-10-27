b28e4dacf73a44609f343a0438db6076d7c2d4fd
prasannasurya12 <prasannasurya12@gmail.com>
2024-10-27 20:31:50 +0530

    AB#33 Developed a standardized approach for constructing request bodies

--- src/Utils/requestBodyFactory.js
+++ src/Utils/requestBodyFactory.js
@@ -2,7 +2,30 @@ import requestBodies from '../model/requestBodies.js'
 
 export default class RequestBodyFactory {
 
-    static createRequestBody(modelName,modelValues){
-      return requestBodies.get(modelName);
+    static reqBodyConfigMap = new Map([
+        ['submit', ['submission', 'progress', 'session']]
+    ]);
+
+    // setting values to their appropriate keys
+    static fillModelBody(model, values) {
+        Object.keys(model).forEach((key, index) => {
+            model[key] = values[index];
+        });
+        return model;
+    }
+
+    static createRequestBody(configName, modelValues) {
+        const requestBody  = {};
+        const configStructure = this.reqBodyConfigMap.get(configName);
+        if (Array.isArray(configStructure)) { // nested object structure
+            configStructure.forEach((modelName, index) => {
+                const modelBody = requestBodies.get(modelName);
+                requestBody[modelName] = {}; // object body created 
+                Object.assign(requestBody [modelName], this.fillModelBody(modelBody, modelValues[index]))
+            }); 
+        } else {
+          requestBody  = this.fillModelBody(requestBodies.get(configStructure), modelValues);
+        }
+        return requestBody ;
     }
 }
--- src/model/requestBodies.js
+++ src/model/requestBodies.js
@@ -1,29 +1,31 @@
-const submitRequestBody = {
-    submission: {
-        submissionId: 0,
-        question: '',
-        status: '',
-        runtime: '', // pass data with + 'ms'
-        language: "Java",
-        submittedQuestion: "",
-        topic: "",
-      },
-      progress: {
-        progressId: 0,
-        question: "",
-        topic: "",
-        mode: "",
-        status: "",
-      },
-      session: {
-        sessionId:0,
-        sessionName: "",
-        mode: "",
-      }
-}
+const submissionRequestBody = {
+    submissionId: 0,
+    question: '',
+    status: '',
+    runtime: '', // pass data with + 'ms'
+    language: "",
+    submittedQuestion: "",
+    topic: "",
+  }
+  
+const sessionRequestBody = {
+    sessionId:0,
+    sessionName: "",
+    mode: "",
+  }
 
+const progressRequestBody = {
+    progressId: 0,
+    question: "",
+    topic: "",
+    mode: "",
+    status: "",
+  }
+  
 const requestPayloads = new Map([
-    ['submit', submitRequestBody]
+    ['submission',submissionRequestBody],
+    ['session',sessionRequestBody],
+    ['progress',progressRequestBody],
 ]);
 
 export default requestPayloads;
