0e20c8ceac1d31dc12641d597311e087b68eafe8
prasannasurya12 <prasannasurya12@gmail.com>
2024-12-08 14:28:04 +0530

    AB#49 Refactored navigation method

--- src/service/httpClient.js
+++ src/service/httpClient.js
@@ -1,21 +1,22 @@
+import ObjectUtils from "@/Utils/object-utils";
 import axios from "axios";
 
 export default class HttpClient {
 
    static setParamValue(url, params) {
         const constructedParams = Object.entries(params)
-        .map(([paramKey,paramValue]) => (paramValue != undefined ? `${paramKey}=${paramValue}` : ''))
+        .map(([paramKey,paramValue]) => (paramValue && paramValue != undefined ? `${paramKey}=${paramValue}` : ''))
         .filter(Boolean).join('&');
         return url.concat("\\").concat('?').concat(constructedParams);
       }
 
       static async executeApiCall(requestMethod, endPoint, requestData) {
-        const { params, reqBody } = requestData;
-        if (params != null && params != undefined) {
+        const { params , reqBody } = requestData || {};
+        if (!ObjectUtils.isNullOrUndefinedOrEmpty(params)) {
           endPoint = HttpClient.setParamValue(endPoint, params);
         }
         let argument = [endPoint];
-        if (reqBody != null) argument.push(reqBody);
+        if (!ObjectUtils.isNullOrUndefinedOrEmpty(reqBody)) argument.push(reqBody);
         try { 
            return await axios[requestMethod](...argument); 
         }
